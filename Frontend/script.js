50856951a49b2b97ca321401a0eede0483bb59e0
unknown <rocj1209@gmail.com>
2023-11-28 17:55:18 -0400

    Initial commit (frontend)

---
+++ Frontend/script.js
@@ -0,0 +1,15 @@
+let buscadorIcono = document.getElementById("buscador-icono");
+let formularioBuscador = document.getElementById("formulario-buscador");
+
+buscadorIcono.addEventListener("click", ()=> {
+    buscadorIcono.style.display = "none";
+    formularioBuscador.style.display = "block";
+
+});
+
+
+
+formularioBuscador.addEventListener("click", ()=> {
+    
+
+});

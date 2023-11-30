e7037b844a442817ebe667e697a3b907d9f45e92
unknown <rocj1209@gmail.com>
2023-11-29 21:45:12 -0400

    Cambios en las medidas

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,13 @@
 let buscadorIcono = document.getElementById("buscador-icono");
 let formularioBuscador = document.getElementById("formulario-buscador");
+let btnCerrar = document.getElementById("btn-cerrar");
 
 buscadorIcono.addEventListener("click", ()=> {
     buscadorIcono.style.display = "none";
     formularioBuscador.style.display = "block";
-
 });
 
-
-
-formularioBuscador.addEventListener("click", ()=> {
-    
-
+btnCerrar.addEventListener("click", ()=> {
+    formularioBuscador.style.display = "none";
+    buscadorIcono.style.display = "block";
 });

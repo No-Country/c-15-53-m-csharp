edd7836897919ae69b2a3eede48e8b2ef0481a2f
Jose Crespo <92603352+Jose-C0@users.noreply.github.com>
2023-12-02 20:22:12 +0000

    Actualizacion Frontend desde la rama front-dev

--- Frontend/script.js
+++ Frontend/script.js
@@ -2,12 +2,12 @@ let buscadorIcono = document.getElementById("buscador-icono");
 let formularioBuscador = document.getElementById("formulario-buscador");
 let btnCerrar = document.getElementById("btn-cerrar");
 
-buscadorIcono.addEventListener("click", ()=> {
-    buscadorIcono.style.display = "none";
-    formularioBuscador.style.display = "block";
-});
+// buscadorIcono.addEventListener("click", ()=> {
+//     buscadorIcono.style.display = "none";
+//     formularioBuscador.style.display = "block";
+// });
 
-btnCerrar.addEventListener("click", ()=> {
-    formularioBuscador.style.display = "none";
-    buscadorIcono.style.display = "block";
-});
+// btnCerrar.addEventListener("click", ()=> {
+//     formularioBuscador.style.display = "none";
+//     buscadorIcono.style.display = "block";
+// });

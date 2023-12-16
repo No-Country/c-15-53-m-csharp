fc340c6c557ef85f3d1b82f9b6236f93bf640c25
Julieta <juliet.simos@gmail.com>
2023-12-15 23:49:24 -0300

    agregado nav y tarjeta de carrito

--- Frontend/carrito.js
+++ Frontend/carrito.js
@@ -9,6 +9,13 @@ let msgAgradecimiento = document.querySelector("#msgAgradecimiento");
 let listado = document.querySelector("#listaCompraFinal");
 let form = document.querySelector('#form');
 
+let btnVolverTienda = document.querySelector("#btn-volverATienda").addEventListener('click', function(){
+    // rederijo a la pagina de productos en la vista general cuando este
+    location.replace("productos.html");
+})
+
+
+//titulo 1 2 y 3 de carrito
 let item1 = document.querySelector("item1");
 let item2 = document.querySelector("item2");
 let item3 = document.querySelector("item3");
@@ -21,27 +28,32 @@ function mostrarForm() {
     paso1.classList.add("quitar");
     form.classList.remove("quitar");
     paso2_opciones.classList.remove("quitar");
+
+    btnRadios = document.querySelectorAll("input[type='radio']");;
+    btnRadios.forEach(btnRadio => {
+        btnRadio.addEventListener('change', function () {
+            paso2_2_form.classList.remove("quitar");
+        })
+    });
     btnContinuar.classList.remove("quitar");
-        btnContinuar.addEventListener("click", function() {
+    // btnContinuar.disabled = true; 
+    btnContinuar.addEventListener("click", function () {
         //Ejecuta la función correspondiente al índice actual
         funciones[funcionActual]();
         //Incrementa el índice de la función
+        console.log("funcion actual: " + funcionActual);
         funcionActual++;
-        });
+    });
 
-let funciones = [
+    let funciones = [
         completarForm,
-        enviarForm,
         ultimoMensaje
-];
+    ];
 }
 
 //primera funcion
 function completarForm() {
-    form.classList.remove("quitar");
-
-    paso2_2_form.classList.remove("quitar");
-
+    console.log("1");
     //guardarme la info del form y hacer una funcion para mandarla a una tabla de ventas
     let contenidoForm = new FormData(form);
 
@@ -61,17 +73,25 @@ function completarForm() {
         codigoPostal: codigoPostal
     };
     //aca deberia de devolver el json de la compra a alguna api
+    console.log("esperanddo");
+        // btnContinuar.disabled = false; 
+    enviarForm();
 
 }
 //segunda funcion
-function enviarForm(){
+function enviarForm() {
+    console.log("2");
+
     form.classList.add("quitar");
     listado.classList.remove("quitar");
+    btnContinuar.innerHTML = "Comprar"
     console.log(listado);
     msgAgradecimiento.classList.remove("quitar");
 }
 
-function ultimoMensaje(){
+function ultimoMensaje() {
+    console.log("3");
+
     //agarrar el elemento del mensaje
     let ultimoMenjaje = document.querySelector("#ultimoMensaje");
     //agregar la clase que lo posiciona
@@ -82,5 +102,4 @@ function ultimoMensaje(){
     //quitarle la clase que lo esconde
     ultimoMenjaje.classList.remove("quitar");
     let body = document.querySelector("#contenedorDesabilitado");
-    body.classList.add("opacado");
 }
